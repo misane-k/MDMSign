f0ff911fa68b7e618f9fd073c5092e8014adf964
crystal <1358154562@qq.com>
2026-01-28 15:22:52 +0800

    allow addQuery/addBody

--- ApiRequest.py
+++ ApiRequest.py
@@ -2,38 +2,36 @@ import time
 import uuid
 import base64
 from hashlib import sha256
-import json
 import hmac
 import requests
+
 
 def time13():
     return int(round(time.time() * 1000))
 def time16():
     return int(round(time.time() * 1000000))
 
+def dict2query(d):
+    if not d:
+        return ""
+    if type(d) != dict:
+        return d
+    sorted_params = sorted(d.items(), key=lambda i: i[0])
+    return "&".join([f"{i[0]}={i[1]}" for i in sorted_params])
+
+
 class ApiRequest:
     scheme = "http"
     host = "api.changyan.com"
     httpPort = 80
-    httpsPort = 443
 
     stage = "RELEASE"
-    publicKey = "From Epas Support Group"
     appSecret = "9dfa99db6daa077b"
     randomKey = ""
     appId = "zhkt-mdm-service"
     contentMD5 = ""
     token = ""
     groupId = ""
-
-    def sign(self, appSecret, url, randomKey, nonce, timeStamp, appId, contentMD5):
-        values = [
-            url, randomKey, nonce, timeStamp, appId, contentMD5
-        ]
-        stringToSign = self.buildStringToSign(values)
-        key = appSecret.encode("utf-8")
-        final = base64.b64encode(hmac.new(key, stringToSign.encode("utf-8"), digestmod=sha256).digest())
-        return str(final, 'utf-8')
 
     def buildStringToSign(self, headValues):
         sb = ""
@@ -42,28 +40,42 @@ class ApiRequest:
                 sb = sb + (headValue + "|")
         stringToSign = sb[0:-1]
         return stringToSign
-    
-    def dictToStr(self, params):
-        sb = []
-        for k,v in params.items():
-            sb.append(f"{k}={v}")
-        return "&".join(sb)
 
-    def syncRequest(self, httpMethod, apiName, params):
-        paramStr = params if type(params) == str else self.dictToStr(params)
-        if httpMethod == "GET" or httpMethod == "POSTQ":
-            url = apiName + "?" + paramStr
+    def sign(self, appSecret, url, randomKey, nonce, timeStamp, appId, contentMD5):
+        values = [url, randomKey, nonce, timeStamp, appId, contentMD5]
+        stringToSign = self.buildStringToSign(values)
+        key = appSecret.encode("utf-8")
+        final = base64.b64encode(hmac.new(key, stringToSign.encode("utf-8"), digestmod=sha256).digest())
+        # print(stringToSign)
+        return str(final, 'utf-8')
+    
+    def __init__(self, httpMethod, apiName):
+        self.httpMethod = httpMethod
+        self.apiName = apiName
+        self.queryParams = {}
+        self.bodyParams = {}
+  
+    def addQuery(self, d):
+        self.queryParams.update(d)
+
+    def addBody(self, d):
+        if type(d) == dict:
+            self.bodyParams.update(d)
         else:
-            url = apiName
-        uri_raw = self.scheme + "://" + self.host + ":" + str(self.httpPort) + apiName
+            self.bodyParams = d
+
+    def syncRequest(self):
         nonce = str(uuid.uuid4())
         timeStamp = str(time13())
-        sign = self.sign(self.appSecret, url, self.randomKey, nonce, timeStamp,
+        url_sign = self.apiName
+        if self.queryParams:
+            self.queryParams = dict2query(self.queryParams)
+            url_sign += "?" + self.queryParams
+        sign = self.sign(self.appSecret, url_sign, self.randomKey, nonce, timeStamp,
                          self.appId, self.contentMD5)
         headers = {
             "Content-type": "application/json; charset=UTF-8",
             "User-Agent": "ShieldJavaSDK",
-
             "S-Auth-Stage": self.stage,
             "S-Auth-AppId": self.appId,
             "S-Auth-Timestamp": timeStamp,
@@ -73,13 +85,26 @@ class ApiRequest:
             "S-Auth-Token": self.token,
             "S-Auth-GroupId": self.groupId
         }
-        if httpMethod == "GET":
-            return requests.get(url=uri_raw, headers=headers, params=params).text
-        elif httpMethod == "POSTQ":
-            # 实际上参数位置分Query和Form，这里是简化处理，认为post参数都在Query
-            return requests.post(url=uri_raw, headers=headers, params=params).text
-        else:
-            return requests.post(url=uri_raw, headers=headers, data=params).text
 
-    # def __init__(self):
-    #     print("fsp loaded")
+        uri = self.scheme + "://" + self.host + ":" + str(self.httpPort) + self.apiName
+        # print(uri)
+        text = requests.request(
+            method=self.httpMethod, url=uri, headers=headers,
+            params=self.queryParams, data=self.bodyParams
+        ).text
+        return text
+
+
+if __name__ == "__main__":
+    api = ApiRequest("GET", "/futureclass/appstore/v2/findForceUpgrade")
+    api.appId = "YFil5acT"
+    api.appSecret = "9mzUQjwEUeuUs6mVz6aegJRZHiFEDASs"
+    api.host = "xktptapi.changyan.com"
+    api.token = ""
+    api.addQuery({
+        "schoolId": "1010000001000024747",
+        "appGroupId": "5.0",
+    })
+    j = api.syncRequest()
+    print(j)
+
--- MDMSign.py
+++ MDMSign.py
@@ -1,9 +1,30 @@
-from urllib import parse
-from ApiRequest import *
-
 import hashlib
+from urllib import parse
+import json
+import ApiRequest
+
+settings = {
+    "platformId": "563542604814225408",
+    "devId": "BZT-W09",
+    "authorizePro":"p_qzk",
+    "classId":"1500000100077460944",
+    "schoolId":"1500000100047144457"
+}
+
+def dict2json(params):
+    gson = {}
+    for param in params:
+        if ":" in param:
+            # only for simple condition like (year:1937)
+            param = param.split(":")
+            gson.update({param[0]:param[1]})
+        elif param in settings:
+            gson.update({param:settings[param]})
+    return json.dumps(gson, separators=(',',':'), sort_keys=True)
+
 class md5():
     md5Hex = None
+
     def getBytes(self):
         origin = self.md5Hex
         result = []
@@ -15,99 +36,66 @@ class md5():
                 result.append(int_hex)
                 s = ""
         return result
+
     def update(self, s):
         s = str(s).encode("utf-8")
         m = hashlib.md5(s)
         self.md5Hex = m.hexdigest()
+    
     def __str__(self):
         return str(self.md5Hex)
+    
     def __init__(self, *args):
         if len(args) > 0:
             self.update(args[0])
 
-import time
-def time13():
-    return int(round(time.time() * 1000))
-def time16():
-    return int(round(time.time() * 1000000))
+def hexMD5(strToMD5):
+    digest = md5(strToMD5).getBytes()
+    sb = ""
+    for b2 in digest:
+        hexString = hex(b2 & 255)[2:]
+        if 1 == len(hexString):
+            hexString = "0" + hexString
+        sb += hexString
+    return str(sb)
 
+def _core(strToSign):
+    # time64 is faster than random number
+    salt = str(str(ApiRequest.time16()))
+    hexmd5 = hexMD5(strToSign + salt)
+    cArr = [None] * 48
+    if hexmd5 != None:
+        i2 = 0
+        while i2 < 48:
+            i3 = i2 // 3
+            i4 = i3 * 2
+            cArr[i2] = hexmd5[i4]
+            cArr[i2 + 1] = salt[i3]
+            cArr[i2 + 2] = hexmd5[i4 + 1]
+            i2 += 3
+    cArr = "".join(cArr)
+    return cArr
 
-class MDMSign:
-    settings = {
-        "platformId": "563542604814225408",
-        "devId": "BZT-W09",
-        "authorizePro":"p_qzk",
-        "classId":"1500000100279433085",
-        "schoolId":"1500000100047767375"
-    }
+def sign(params: dict) -> str:
+    params["timeStamp"] = str(ApiRequest.time13())
+    params["sign"] = _core(ApiRequest.dict2query(params).lower())
+    return parse.urlencode(params)
 
-    def paramToString(self, treeMap):
-        sb = ""
-        for i in treeMap:
-            sb += str(i)
-            sb += "="
-            sb += str(treeMap[i])
-            sb += "&"
-        if len(sb) <= 1:
-            return ""
-        sb = sb[:-1]
-        return str(sb).lower()
-    
-    # def paramToString(self, treeMap):
-    #     return "&".join([f"{k}={v}" for k,v in treeMap.items()]).lower()
-
-    def paramsToQueryJson(self, params):
-        gson = {}
-        for param in params:
-            if ":" in param:
-                # only for simple condition like (year:1937)
-                param = param.split(":")
-                gson.update({param[0]:param[1]})
-            elif param in self.settings:
-                gson.update({param:self.settings[param]})
-        return json.dumps(gson, separators=(',',':'), sort_keys=True)
-
-    def core(self, strToSign):
-        # time64 is faster than random number
-        salt = str(str(time16()))
-        hexmd5 = self.hexMD5(strToSign + salt)
-        cArr = [None] * 48
-        if hexmd5 != None:
-            i2 = 0
-            while i2 < 48:
-                i3 = i2 // 3
-                i4 = i3 * 2
-                cArr[i2] = hexmd5[i4]
-                cArr[i2 + 1] = salt[i3]
-                cArr[i2 + 2] = hexmd5[i4 + 1]
-                i2 += 3
-        cArr = "".join(cArr)
-        return cArr
-
-    def hexMD5(self, strToMD5):
-        digest = md5(strToMD5).getBytes()
-        sb = ""
-        for b2 in digest:
-            hexString = hex(b2 & 255)[2:]
-            if 1 == len(hexString):
-                hexString = "0" + hexString
-            sb += hexString
-        return str(sb)
-
+def mdm_req(uri, treeMap):
+    if treeMap.get("platformId") == "1315934342538678272":
+        uri = uri.replace("/mdm/", "/smdm/")
+    body = sign(treeMap)
+    api = ApiRequest.ApiRequest("POST", uri)
+    api.addBody(body)
+    text = api.syncRequest()
+    return json.loads(text)
 
 
 if __name__ == "__main__":
-    self = MDMSign()
-    timeStamp = str(time13())
     args = ["platformId", "devId", "authorizePro", "classId", "schoolId"]
-    treeMap = {}
-    treeMap.update({
-        "param": self.paramsToQueryJson(args),
-        "timeStamp": timeStamp
-    })
-    sign = self.core(self.paramToString(treeMap))
-    treeMap.update({"sign": sign})
+    treeMap = {"param": dict2json(args)}
+    body = sign(treeMap)
 
-    api = ApiRequest()
-    response = api.syncRequest("POST", "/mdm/dev/queryApplicationByConditionV7", parse.urlencode(treeMap))
-    print(response)
+    api = ApiRequest.ApiRequest("POST", "/mdm/dev/queryApplicationByConditionV7")
+    api.addBody(body)
+    print(api.syncRequest())
